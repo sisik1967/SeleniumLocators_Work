0ced1180cbc44e024fc2c4d2a63da50f751d3009
sisik1967 <sabahattin67@gmail.com>
2022-12-14 23:24:35 -0600

    updated

--- src/main/java/MyWebAppAutomationProgram.java
+++ src/main/java/MyWebAppAutomationProgram.java
@@ -14,6 +14,7 @@ public class MyWebAppAutomationProgram extends MyMethods {
     public static void main(String[] args) throws InterruptedException {
         Constants obje=new Constants();
 
+        MyElements elements = new MyElements();
 
         //Define elementActions using MyWebElements class
         MyMethods elementActions = new MyMethods();
@@ -22,7 +23,7 @@ public class MyWebAppAutomationProgram extends MyMethods {
         elementActions.launchBrowser(obje.URL);
 
        // Locate WebElement search box in Amazon.com
-        elementActions.locateEl(new MyElements().element01);
+        elementActions.locateEl(elements.element01);
 
         //Send searchItem (16TB External Hard Drive SSD) to Search Box in Amazon.com
         elementActions.searchElement(obje.SEARCHITEM);
@@ -32,45 +33,45 @@ public class MyWebAppAutomationProgram extends MyMethods {
 
 
         //Locate the xpath link text to select the desired item
-        elementActions.loacateXpathLinkText(new MyElements().element02);
+        elementActions.loacateXpathLinkText(elements.element02);
 
         //Click the located item to see the item
         elementActions.clickLocatedItem();
         Thread.sleep(1000);
 
         //Find or locate Add to Cart button/link
-        elementActions.findAddToCartButton(new MyElements().element03);
+        elementActions.findAddToCartButton(elements.element03);
         Thread.sleep(1000);
 
         //Click the located item to add the item to the shopping cart.
         elementActions.clickLocatedItem();
 
        //Find or locate Proceed To Checkout Button
-        elementActions.findGoToChart(new MyElements().element06);
+        elementActions.findGoToChart(elements.element06);
 
         //Click the located item
         elementActions.clickLocatedItem();
 
         //Find or locate Proceed To Checkout Button
-        elementActions.findProceedToCheckoutButton(new MyElements().element07);
+        elementActions.findProceedToCheckoutButton(elements.element07);
 
         //Click the located item
         elementActions.clickLocatedItem();
 
         //Find or locate MailBox
-        elementActions.findMailBox(new MyElements().element08);
+        elementActions.findMailBox(elements.element08);
 
         //Send Email from Constants
         elementActions.searchElement(obje.EMAIL);
 
         //Find or locate Continue
-        elementActions.findContinue(new MyElements().element09);
+        elementActions.findContinue(elements.element09);
 
         //Click the located item
         elementActions.clickLocatedItem();
 
         //Find or locate password
-        elementActions.findPassword(new MyElements().element10);
+        elementActions.findPassword(elements.element10);
 
         //Send password from Constants
         elementActions.searchElement(obje.PASSWORD);
